68dda2ac343b67b1bc02c50281b857375d1baa36
Hassan Raza <hassan.raza@shipsy.co.in>
2021-05-03 00:24:42 +0530

    added bundling and refractoring

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,4 +1,44 @@
-const { config } = require('./config.json');
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((module) => {
+
+"use strict";
+module.exports = JSON.parse('{"config":[{"row":5,"col":5,"rewardPt":10,"deductionPt":5,"highlightDuration":800,"passingScore":50},{"row":10,"col":10,"rewardPt":10,"deductionPt":5,"highlightDuration":600,"passingScore":100}]}');
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+const { config } = __webpack_require__(1);
 
 // remove wrong move animation image
 function removeFailureImage(block) {
@@ -183,3 +223,7 @@ function loadGame() {
 }
 
 loadGame()
+})();
+
+/******/ })()
+;
---
+++ readme.md
@@ -0,0 +1,28 @@
+## use dist/index.html file to play the game. Please read below section for explaination of the game.
+
+---
+# Tik Tok Game
+
+### Design a tik tok game which will be driven completely by a config. Each stage of the game will have an n x n matrix of boxes. Following are the rules :-
+
+#
+
+1. One box in the matrix will be highlighted till the user clicks on it or the time gets over.
+   Highlighted boxes will keep on changing randomly.
+2. If the user clicks on the highlighted matrix, he will be rewarded with some points given in the configuration.
+3. If the user clicks on a non-highlighted matrix, some points will be deducted from his total score.
+4. Every stage will have a passing score (Given in the configuration).
+5. If the user reaches the passing score, the next stage of the game will be triggered.
+6. Once the user passes the last stage, we will have to congratulate him with some animation.
+
+#
+
+## Note:
+
+##### Use only native/vanilla JS. Please refrain from using any external plugins, frameworks or libraries.
+
+##### Code quality should be at a production code level.
+
+##### Implementation should be working without any errors.
+
+#
